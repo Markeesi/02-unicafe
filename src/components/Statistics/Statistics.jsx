75ea1f8abf9adfa28c6c7d7f0ecf5f9228ccad1c
Markeesi <62094844+Markeesi@users.noreply.github.com>
2023-10-04 00:04:50 +0300

    pieni fiksaus varoitukseen

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,14 +2,16 @@ import StatisticLine from "../StatisticLine/StatisticLine";
 
 const Statistics = ({ good, neutral, bad, all, average, positive }) => {
   return (
-    <>
+    <table>
+    <tbody>
       <StatisticLine text="good" value={good} />
       <StatisticLine text="neutral" value={neutral} />
       <StatisticLine text="bad" value={bad} />
       <StatisticLine text="all" value={all} />
       <StatisticLine text="average" value={average} />
       <StatisticLine text="positive" value={positive} />
-    </>
+      </tbody>
+    </table>
   );
 };
 
