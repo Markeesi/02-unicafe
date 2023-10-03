d34b72324576d0cf492dd229f4af439a79539f82
Markeesi <62094844+Markeesi@users.noreply.github.com>
2023-10-03 22:28:23 +0300

    1.7 tehtävä tehty

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,10 @@ const App = () => {
   const badCounter = () => setBad(bad +1);
   const neutralCounter = () => setNeutral(neutral +1);
 
+   const all = good + neutral + bad;
+   const average = (good - bad) / all; 
+   const positive = good / all * 100;
+
   return (
     <>
     <div>
@@ -25,6 +29,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
     </>
   )
